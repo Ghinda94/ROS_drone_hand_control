50459476d92d511aee44d71ac44891500f16bc93
Ghinda94 <bogdan.p.deac@gmail.com>
2016-06-29 16:43:12 +0300

    First commit

---
+++ src/droneTakeOff.cpp
@@ -0,0 +1,28 @@
+#include "ros/ros.h"
+#include <std_msgs/Empty.h>
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "droneTakeOff");
+
+  ros::NodeHandle n;
+
+  ros::Publisher droneTakeOff_pub = n.advertise<std_msgs::Empty>("ardrone/takeoff", 1000);
+
+  ros::Rate loop_rate(10);
+
+  while (ros::ok())
+  {
+
+    std_msgs::Empty msg;
+
+    droneTakeOff_pub.publish(msg);
+
+    ros::spinOnce();
+
+  }
+
+
+  return 0;
+}
