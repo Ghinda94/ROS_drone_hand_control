6de067c67091513d37fe816db4f1b5225b1495b1
Ghinda94 <bogdan.p.deac@gmail.com>
2016-06-30 11:55:14 +0300

    Publishers for takeoff and land are ready and functional. They are just for test. Don't work with the camera.

---
+++ src/droneLand.cpp
@@ -0,0 +1,28 @@
+#include "ros/ros.h"
+#include <std_msgs/Empty.h>
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "droneLand");
+
+  ros::NodeHandle n;
+
+  ros::Publisher droneLand_pub = n.advertise<std_msgs::Empty>("ardrone/land", 1000);
+
+  ros::Rate loop_rate(10);
+
+  while (ros::ok())
+  {
+
+    std_msgs::Empty msg;
+
+    droneLand_pub.publish(msg);
+
+    ros::spinOnce();
+
+  }
+
+
+  return 0;
+}
